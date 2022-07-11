5db9cccd52c9668ffd80d73ad02d4d9d3acf13e2
Ravi Shankar <40320328+Ravilkr14@users.noreply.github.com>
2022-07-11 18:30:33 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -1,11 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"github.com/shomali11/slacker"
+)
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel{
+		fmt.Println("command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
+
 
 func main() {
-	fmt.Println("HELLO WORLD")
-	fmt.Println("golang")
-	a := 5
-	b := 2
-	fmt.Println(a*b)
+	os.Setenv("SLACK_BOT_TOKEN","xoxb-3771061728292-3762089810310-jlDuXaPjjobMiABQI8ez5juC")
+	os.Setenv("SLACK_APP_TOKEN","xapp-1-A03NLHKCM5Z-3768601860659-8410f0fcc03843ea5cba198c24769e9b49b5b82cf0a7d15f63e902a6fc4a0072")
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("my yob is <year>", &slacker.CommandDefinition{
+		Description: "yob calculator",
+		Example: "my yob is 2020",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request,response slacker.ResponseWriter){
+		 	year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err!= nil {
+				println("error")
+			}
+			age := 2022-yob
+			r := fmt.Sprintf("age is %d", age)
+			response.Reply(r)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil{
+		log.Fatal(err)
+	}
 }
+
